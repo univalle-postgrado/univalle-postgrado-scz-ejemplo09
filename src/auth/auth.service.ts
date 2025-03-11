e1d3327b3230f94423ed2778e2fbf5ac0137daec
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-03-11 16:29:39 -0400

    Se instala bcrypt para cifrar la contraseña, se valida que el login o email no sean duplicados y se establece prefrase para hacer más fuerte el cifrado

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from 'src/users/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private configService: ConfigService,
     @InjectRepository(User)
     private usuariosRepository: Repository<User>,
   ) {}
 
-  register(createUserDto: CreateUserDto) {
+  async register(createUserDto: CreateUserDto) {
+    const existsUser = await this.usuariosRepository.exists({
+      where: [
+        { login: createUserDto.login },
+        { email: createUserDto.email },
+      ]
+    });
+    if (existsUser) {
+      throw new ConflictException('El login o email ya se encuentra registrado con otro usuario');
+    }
+    // saltRounds = 10, es recomendable ya que mayor valor aumenta la complejidad del hash pero también el uso de procesador
+    const hashedPassword = await bcrypt.hash(this.configService.get<string>('ENCRYPTION_KEY') + createUserDto.password, 10);
     const newUser = this.usuariosRepository.create({  
       ...createUserDto,
+      password: hashedPassword,
       last_access: new Date()
     });
     return this.usuariosRepository.save(newUser);
