3c439dc433f374a0c6542e00e894d2ff9ba331e4
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-03-11 18:09:32 -0400

    Configuración por defecto de Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     stopAtFirstError: true,
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('MOVIES API')
+    .setDescription('API Reference')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('PORT') || 3000);
