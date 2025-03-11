bc7cf2656419da21aaaf527391205f50de62d057
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-03-11 15:53:46 -0400

    Modificación del módulo de users y definición de propiedades de la entidad user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+@Unique(['login'])
+@Unique(['email'])
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 30 })
+    login: string;
+
+    @Column({ length: 300, select: false })
+    password: string;
+
+    @Column({ length: 150 })
+    fullname: string;
+
+    @Column({ length: 150 })
+    email: string;
+
+    @Column({ length: 30, nullable: true })
+    phone: string;
+
+    @Column({ type: 'timestamp without time zone', nullable: true })
+    last_access: Date | null;
+
+    @Column({ default: false })
+    enabled: boolean;
+
+    @CreateDateColumn({ type: 'timestamp without time zone', select: false })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: 'timestamp without time zone', select: false, nullable: true })
+    updated_at: Date;
+
+}
