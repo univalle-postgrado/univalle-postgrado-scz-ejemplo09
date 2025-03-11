4e6f729efe7594e0ff73f8878d5ac7cc2b9c7a98
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-03-11 18:28:25 -0400

    Configuración de propiedades en de los DTOs para el Swagger

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDto {
 
+    @ApiProperty({
+        description: 'Login de usuario',
+        example: '',
+      })
     @IsNotEmpty({ message: 'Login no puede ser un valor vacío' })
     @IsString({ message: 'Login debe ser una cadena de texto' })
     login: string;
 
+    @ApiProperty({
+        description: 'Contraseña de usuario',
+        example: '',
+      })
     @IsNotEmpty({ message: 'Password no puede ser un valor vacío' })
     @IsString({ message: 'Password debe ser una cadena de texto' })
     password: string;
