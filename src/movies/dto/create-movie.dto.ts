48ed638df916fe7d3a9de6d1897f87cee0c7e98e
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-03-13 14:56:53 -0400

    Decorators para Movies

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsDateString,
   IsNotEmpty,
@@ -10,32 +11,60 @@ import {
 } from 'class-validator';
 
 export class CreateMovieDto {
+  @ApiProperty({
+      description: 'Título de Película',
+      example: 'La vida es bella',
+    })
   @IsNotEmpty({ message: 'Título no puede estar vacío' })
   @IsString({ message: 'Título debe ser una cadena de texto' })
   title: string;
 
+  @ApiProperty({
+      description: 'Sinopsis',
+      example: 'En 1939, a punto de estallar la Segunda Guerra Mundial (1939-1945), el extravagante Guido llega a Arezzo, en la Toscana,...',
+    })
   @IsNotEmpty({ message: 'Sinopsis no puede estar vacía' })
   @IsString({ message: 'Sinopsis debe ser una cadena de texto' })
   synopsis: string;
 
+  @ApiProperty({
+    description: 'Director',
+    example: 'Roberto Benigni',
+  })
   @IsNotEmpty({ message: 'Director no puede estar vacío' })
   @IsString({ message: 'Director debe ser una cadena de texto' })
   director: string;
 
+  @ApiProperty({
+    description: 'Fecha de estreno',
+    example: '1997-12-20',
+  })
   @IsDateString(
     {},
     { message: 'Fecha de estreno debe ser de tipo fecha YYYY-MM-DD' },
   )
   releaseDate: Date;
 
+  @ApiProperty({
+    description: 'URL de la portada',
+    example: 'https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg',
+  })
   @IsUrl({}, { message: 'La URL de la imagen debe tener formato URI' })
   posterUrl: string;
 
+  @ApiProperty({
+    description: 'Valoración',
+    example: '8.5',
+  })
   @IsNumber({}, { message: 'Valoración debe ser un valor numérico' })
   @Min(0.0)
   @Max(10.0)
   rating: number;
 
+  @ApiProperty({
+    description: 'ID de Categoría',
+    example: '3',
+  })
   @IsNumber({}, { message: 'Id de Categoría debe ser un valor numérico' })
   @IsPositive({ message: 'Id de Categoría debe ser un número entero positivo' })
   categoryId: number;
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -14,7 +14,14 @@ import {
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiBearerAuth('access-token')
 @ApiTags('movies')
@@ -22,11 +29,32 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
+  @ApiOperation({ summary: 'Crear una Película' })
+  @ApiResponse({
+    status: 201,
+    description: 'Película creada',
+  })
   @Post()
   create(@Body() createMovieDto: CreateMovieDto) {
     return this.moviesService.create(createMovieDto);
   }
 
+  @ApiOperation({ summary: 'Obtener la lista de todas las Películas' })
+  @ApiQuery({ name: 'page', description: 'Nro de página', example: 1 })
+  @ApiQuery({
+    name: 'limit',
+    description: 'Cantidad de resultados páginados',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'relations',
+    description: 'Obtener Categoría relacionada',
+    example: false,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Listado de Películas',
+  })
   @Get()
   findAll(
     @Query('page') page: number,
@@ -37,6 +65,16 @@ export class MoviesController {
     return this.moviesService.findAll(page, limit, relations);
   }
 
+  @ApiOperation({
+    summary: 'Obtener los datos de una Película en base a su ID',
+  })
+  @ApiParam({ name: 'id', description: 'ID de Película' })
+  @ApiQuery({
+    name: 'relations',
+    description: 'Si se requiere obtener su relación con Categoría',
+  })
+  @ApiResponse({ status: 200, description: 'Película encontrada' })
+  @ApiResponse({ status: 404, description: 'Película no encontrada' })
   @Get(':id')
   findOne(
     @Param('id') id: number,
@@ -46,17 +84,42 @@ export class MoviesController {
     return this.moviesService.findOne(id, relations);
   }
 
+  @ApiOperation({
+    summary: 'Actualizar los datos de una Película en base a su ID',
+  })
+  @ApiParam({ name: 'id', description: 'ID de Película' })
+  @ApiResponse({ status: 200, description: 'Película actualizada' })
+  @ApiResponse({
+    status: 404,
+    description: 'Película no actualizada porque no existe',
+  })
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateMovieDto: UpdateMovieDto) {
     return this.moviesService.update(id, updateMovieDto);
   }
 
+  @ApiOperation({ summary: 'Eliminar una Película en base a su ID' })
+  @ApiParam({ name: 'id', description: 'ID de Película' })
+  @ApiResponse({ status: 204, description: 'Película eliminada' })
+  @ApiResponse({
+    status: 404,
+    description: 'Película no eliminada porque no existe',
+  })
   @Delete(':id')
   @HttpCode(204)
   async remove(@Param('id') id: number) {
     await this.moviesService.remove(id);
   }
 
+  @ApiOperation({
+    summary: 'Obtener la Categoría de una Película en base a su ID',
+  })
+  @ApiParam({ name: 'id', description: 'ID de Película' })
+  @ApiResponse({
+    status: 200,
+    description: 'Categoría asociada al ID de Película',
+  })
+  @ApiResponse({ status: 404, description: 'Película no encontrada' })
   @Get(':id/category')
   findCategory(@Param('id') id: number) {
     return this.moviesService.findCategory(id);
